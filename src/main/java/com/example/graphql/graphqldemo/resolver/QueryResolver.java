7f44349cbc84d9c48445b44730cf49df4e41fdf4
Matt LaDuca <foobar402@gmail.com>
2018-12-10 22:53:26 -0600

    getting token from DataFetchingEnvironment optional parameter

--- src/main/java/com/example/graphql/graphqldemo/resolver/QueryResolver.java
+++ src/main/java/com/example/graphql/graphqldemo/resolver/QueryResolver.java
@@ -4,19 +4,32 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.example.graphql.graphqldemo.domain.Company;
 import com.example.graphql.graphqldemo.domain.DB;
 import com.example.graphql.graphqldemo.domain.Job;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.servlet.GraphQLContext;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
 
-    public Job getJob(String id) {
+    public Job getJob(String id, DataFetchingEnvironment env) {
+        Object context = env.getContext();
         return DB.getJob(id);
     }
 
-    public List<Job> getJobs() {
+    public List<Job> getJobs(DataFetchingEnvironment env) {
+        GraphQLContext context = env.getContext();
+        Optional<HttpServletRequest> httpServletRequest = context.getHttpServletRequest();
+        String token = httpServletRequest
+                .map(req -> req.getHeader("Authorization"))
+                .filter(header -> !header.isEmpty())
+                .map(header -> header.replace("Bearer ", ""))
+                .map(String::toString)
+                .orElse(null);
         return DB.getJobs();
     }
 
